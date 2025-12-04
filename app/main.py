a8ed45c048cc90e289905410e4f3bc5c6ab5914e
Nikhil Murade <muradenikhil@gmail.com>
2025-12-04 21:02:46 +0530

    update middleware

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ _start_time = time.time()
 # -------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
